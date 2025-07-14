e2470ed08bf98e8076412ba32d9e19015a7fb04c
Marius Breivik <marius.breivik@spk.no>
2025-07-14 16:12:06 +0200

    chore(eslint): migrate eslint configuration

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+/** @type {import("eslint").Linter.FlatConfig[]} */
+export default [
+    js.configs.recommended,
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        languageOptions: {
+            parser: tsParser,
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module",
+            },
+        },
+        plugins: {
+            "@typescript-eslint": tseslint,
+        },
+        rules: {
+            ...tseslint.configs.recommended.rules,
+            "@typescript-eslint/no-explicit-any": "off",
+        },
+    },
+    {
+        languageOptions: {
+            globals: {
+                jest: "readonly",
+            },
+        },
+    },
+];
